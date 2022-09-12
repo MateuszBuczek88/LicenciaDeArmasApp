cc2d191ed0113e028e64e9441c659e182d0a31a4
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-12 11:50:18 +0200

    Create HomeScreenFragment, set home destination

---
+++ app/src/main/java/com/example/licenciadearmas/HomeScreenFragment.kt
@@ -0,0 +1,24 @@
+package com.example.licenciadearmas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+
+class HomeScreenFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+            }
+        }
+    }
+}
