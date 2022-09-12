95d1ba66fa2dc9a28d5ba85b93269ca2ba8d8613
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-12 11:36:34 +0200

    make composable work with nav component

--- app/src/main/java/com/example/licenciadearmas/MainActivity.kt
+++ app/src/main/java/com/example/licenciadearmas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.licenciadearmas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -19,75 +20,9 @@ import com.example.licenciadearmas.data.questions.*
 
 import com.example.licenciadearmas.ui.theme.LicenciaDeArmasTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LicenciaDeArmasTheme(darkTheme = false) {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column() {
-                        QuestionCard(
-                            question = questionRepo.getQuestionList(Sections.Tema1).first()
-                        )
-
-                    }
-                }
-            }
-        }
-    }
-}
-
-val questionRepo = QuestionRepository(
-    questionsTema1,
-    questionsTema2,
-    questionsTema3,
-    questionsTema4,
-    questionsTema5,
-    questionsTema6,
-    questionsTema7
-)
-
-@Composable
-fun QuestionCard(question: Question) {
-    Column() {
-        Surface(
-            shape = MaterialTheme.shapes.small,
-            elevation = 1.dp,
-            modifier = Modifier.padding(all = 6.dp)
-        ) {
-            Text(text = question.text, style = MaterialTheme.typography.h6)
-        }
-        Spacer(modifier = Modifier.height(4.dp))
-        question.answersList.forEach {
-            Surface(
-                shape = MaterialTheme.shapes.small,
-                elevation = 1.dp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable { }) {
-                Text(text = it, modifier = Modifier.padding(all = 12.dp))
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewQuestion() {
-    LicenciaDeArmasTheme {
-        val question = Question(
-            "El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos\n" +
-                    "sucesivos accionando el disparador una sola vez, se denomina: ",
-            listOf(
-                "A).-Arma de fuego semiautomática",
-                "B).-Arma de fuego de repetición",
-                "C).-Arma de fuego automática"
-            ),
-            "C).-Arma de fuego automática"
-        )
-
+        setContentView(R.layout.activity_main)
     }
 }
