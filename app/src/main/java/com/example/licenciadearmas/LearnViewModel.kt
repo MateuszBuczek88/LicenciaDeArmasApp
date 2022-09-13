aac4279cc4b1327e12084b5d6c7fe9671209e7bb
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-13 21:24:17 +0200

    LearnViewModel implementation

--- app/src/main/java/com/example/licenciadearmas/LearnViewModel.kt
+++ app/src/main/java/com/example/licenciadearmas/LearnViewModel.kt
@@ -1,8 +1,28 @@
 package com.example.licenciadearmas
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.licenciadearmas.data.IQuestionRepository
+import com.example.licenciadearmas.data.Question
 import com.example.licenciadearmas.data.Sections
 
-class LearnViewModel(val repository: IQuestionRepository,val section :Sections): ViewModel() {
+class LearnViewModel(val repository: IQuestionRepository, val section: Sections) : ViewModel() {
+    val questionList: MutableList<Question> = repository.getQuestionList(section).toMutableList()
+    val _question  =  MutableLiveData<Question>(questionList.first())
+    val question: LiveData<Question>
+        get() = _question
+
+
+    fun nextQuestion() {
+        _question.value?.let { questionList.add(it) }
+        questionList.removeFirst()
+        _question.value = questionList.first()
+    }
+
+    fun prevQuestion() {
+        _question.value = questionList.last()
+        question.value?.let { questionList.add(0, it) }
+        questionList.removeLast()
+    }
 }
