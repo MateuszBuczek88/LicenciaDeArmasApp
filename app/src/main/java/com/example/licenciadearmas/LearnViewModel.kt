8ebaff255124dad3bb7a2c88d8bd43d18408b848
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-13 12:56:27 +0200

    ViewModel for LearnFragment initial commit

---
+++ app/src/main/java/com/example/licenciadearmas/LearnViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.licenciadearmas
+
+import androidx.lifecycle.ViewModel
+import com.example.licenciadearmas.data.IQuestionRepository
+import com.example.licenciadearmas.data.Sections
+
+class LearnViewModel(repository: IQuestionRepository, section :Sections): ViewModel() {
+}
