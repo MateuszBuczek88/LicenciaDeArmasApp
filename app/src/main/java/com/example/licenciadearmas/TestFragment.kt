ecd542d7ca3d9d0f09be6938b81d105aa1eab2fd
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-15 13:58:07 +0200

    Create QuestionCard and AnsersCard

--- app/src/main/java/com/example/licenciadearmas/TestFragment.kt
+++ app/src/main/java/com/example/licenciadearmas/TestFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.sp
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TestFragment : Fragment() {
-    val viewModel: TestViewModel by viewModel()
+    private val viewModel: TestViewModel by viewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,19 +24,48 @@ class TestFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 val question = viewModel.question.observeAsState()
-                TestContent()
-                question.value?.let {
-                    Text(
-                        text = it.text,
-                        fontSize = 16.sp,
-                        color = Color.White
-                    )
+                question.value?.let { text->
+                    question.value?.let { answers ->
+
+                        TestContent(
+                            text.text, answers.answersList
+                        )
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun TestContent() {
+fun TestContent(
+    questionText: String,
+    answers: List<String>
+) {
+    Column() {
+        QuestionCard(questionText = questionText)
+        AnswersCard(answers = answers)
+
+
+    }
+}
+
+@Composable
+fun QuestionCard(questionText: String) {
+    Surface() {
+        Text(text = questionText)
+    }
+}
+
+@Composable
+fun AnswersCard(answers: List<String>) {
+    Surface() {
+        Column() {
+            answers.forEach {
+                Text(text = it)
+            }
+
+        }
+    }
 }
