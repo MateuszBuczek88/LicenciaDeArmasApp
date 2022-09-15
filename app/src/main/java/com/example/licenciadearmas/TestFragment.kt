b08f3f623449bbe193bafa25dde7d014f124bc9b
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-15 13:40:46 +0200

    async getQuestions for TestViewModel

--- app/src/main/java/com/example/licenciadearmas/TestFragment.kt
+++ app/src/main/java/com/example/licenciadearmas/TestFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.sp
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TestFragment : Fragment() {
@@ -14,11 +19,24 @@ class TestFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        viewModel.getQuestions()
 
         return ComposeView(requireContext()).apply {
             setContent {
+                val question = viewModel.question.observeAsState()
+                TestContent()
+                question.value?.let {
+                    Text(
+                        text = it.text,
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
             }
         }
     }
 }
 
+@Composable
+fun TestContent() {
+}
--- app/src/main/java/com/example/licenciadearmas/TestViewModel.kt
+++ app/src/main/java/com/example/licenciadearmas/TestViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.licenciadearmas
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.licenciadearmas.data.IQuestionRepository
+import com.example.licenciadearmas.data.Question
+import com.example.licenciadearmas.data.Sections
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.awaitAll
 
-class TestViewModel(val repository: IQuestionRepository):ViewModel() {
+class TestViewModel(val repository: IQuestionRepository) : ViewModel() {
+    lateinit var testQuestionsList: MutableList<Question>
+    fun getQuestions() {
+        viewModelScope.launch {
+
+            testQuestionsList = Sections.values().map {
+                async { repository.getQuestionList(it).shuffled().take(it.toTest) }
+            }
+                .awaitAll().flatten().toMutableList()
+
+            _question.value = testQuestionsList.first()
+        }
+    }
+
+    private var _question = MutableLiveData<Question?>()
+    val question: LiveData<Question?>
+        get() = _question
 }
