c32a856936581dc17978b6a043b7f132cdf4ac76
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-16 22:23:56 +0200

    Mocked Questions

--- app/src/main/java/com/example/licenciadearmas/data/questions/QuestionsTema1.kt
+++ app/src/main/java/com/example/licenciadearmas/data/questions/QuestionsTema1.kt
@@ -4,7 +4,7 @@ import com.example.licenciadearmas.data.Question
 
 val questionsTema1 = listOf(
     Question(
-        "El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
+        "11 El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
         listOf(
             "A).-Arma de fuego semiautomática",
             "B).-Arma de fuego de repetición",
@@ -13,7 +13,7 @@ val questionsTema1 = listOf(
         "C).-Arma de fuego automática"
     ),
     Question(
-        "Si el arma de fuego se recarga después de cada disparo, mediante un mecanismo accionado por el tirador,\n" +
+        "12 Si el arma de fuego se recarga después de cada disparo, mediante un mecanismo accionado por el tirador,\n" +
                 "estaremos ante:",
         listOf(
             "A).-Arma de fuego semiautomática",
@@ -21,7 +21,25 @@ val questionsTema1 = listOf(
             "C).-Arma de fuego automática"
         ),
         "B).-Arma de fuego de repetición."
-    )
+    ),
+    Question(
+        "13 El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
+        listOf(
+            "A).-Arma de fuego semiautomática",
+            "B).-Arma de fuego de repetición",
+            "C).-Arma de fuego automática"
+        ),
+        "C).-Arma de fuego automática"
+    ),
+    Question(
+        "14 El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
+        listOf(
+            "A).-Arma de fuego semiautomática",
+            "B).-Arma de fuego de repetición",
+            "C).-Arma de fuego automática"
+        ),
+        "C).-Arma de fuego automática"
+    ),
 
 
 )
--- app/src/main/java/com/example/licenciadearmas/data/questions/QuestionsTema3.kt
+++ app/src/main/java/com/example/licenciadearmas/data/questions/QuestionsTema3.kt
@@ -2,4 +2,44 @@ package com.example.licenciadearmas.data.questions
 
 import com.example.licenciadearmas.data.Question
 
-val questionsTema3 = emptyList<Question>()
+val questionsTema3 =  listOf(
+    Question(
+        "31 El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
+        listOf(
+            "A).-Arma de fuego semiautomática",
+            "B).-Arma de fuego de repetición",
+            "C).-Arma de fuego automática"
+        ),
+        "C).-Arma de fuego automática"
+    ),
+    Question(
+        "32 Si el arma de fuego se recarga después de cada disparo, mediante un mecanismo accionado por el tirador,\n" +
+                "estaremos ante:",
+        listOf(
+            "A).-Arma de fuego semiautomática",
+            "B).-Arma de fuego de repetición.",
+            "C).-Arma de fuego automática"
+        ),
+        "B).-Arma de fuego de repetición."
+    ),
+    Question(
+        "33 El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
+        listOf(
+            "A).-Arma de fuego semiautomática",
+            "B).-Arma de fuego de repetición",
+            "C).-Arma de fuego automática"
+        ),
+        "C).-Arma de fuego automática"
+    ),
+    Question(
+        "34 El arma de fuego que se recarga automáticamente, después de cada disparo, efectuando varios disparos sucesivos accionando el disparador una sola vez, se denomina:",
+        listOf(
+            "A).-Arma de fuego semiautomática",
+            "B).-Arma de fuego de repetición",
+            "C).-Arma de fuego automática"
+        ),
+        "C).-Arma de fuego automática"
+    ),
+
+
+    )
