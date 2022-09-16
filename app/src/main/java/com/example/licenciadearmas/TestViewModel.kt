31e3700e04560c07dbad3abfdbf18c724dd56fd9
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-16 22:35:52 +0200

    getQuestionList working with Result,  logic for answers checking

--- app/src/main/java/com/example/licenciadearmas/TestViewModel.kt
+++ app/src/main/java/com/example/licenciadearmas/TestViewModel.kt
@@ -12,20 +12,57 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.awaitAll
 
 class TestViewModel(val repository: IQuestionRepository) : ViewModel() {
-    lateinit var testQuestionsList: MutableList<Question>
+    init {
+        getQuestions()
+    }
+
     fun getQuestions() {
         viewModelScope.launch {
 
-            testQuestionsList = Sections.values().map {
-                async { repository.getQuestionList(it).shuffled().take(it.toTest) }
+            testQuestionsList = Sections.values().map { sections ->
+                async {
+                    repository.getQuestionList(sections).map { it.shuffled().take(sections.toTest) }
+                }
             }
-                .awaitAll().flatten().toMutableList()
+                    //implement runCatching in let
+                .awaitAll().let { resultList -> resultList.flatMap { it.getOrThrow() }.toMutableList() }
 
             _question.value = testQuestionsList.first()
         }
     }
 
-    private var _question = MutableLiveData<Question?>()
+    lateinit var testQuestionsList: MutableList<Question>
+    private var _question = MutableLiveData<Question?>(null)
     val question: LiveData<Question?>
         get() = _question
+    private var _isAnswerCorrect = MutableLiveData<Boolean>(false)
+    val isAnswerCorrect: LiveData<Boolean>
+        get() = _isAnswerCorrect
+    private var _rightAnswers = MutableLiveData<Int>(0)
+    val rightAnswers: LiveData<Int>
+        get() = _rightAnswers
+    private var _wrongAnswers = MutableLiveData<Int>(0)
+    val wrongAnswers: LiveData<Int>
+        get() = _wrongAnswers
+
+
+    fun checkAnswer(userAnswer: String) {
+        if (question.value?.rightAnswer == userAnswer) {
+            _isAnswerCorrect.value = true
+            _rightAnswers.value = _rightAnswers.value?.plus(1)
+            nextQuestion()
+
+
+        } else {
+            nextQuestion()
+            _wrongAnswers.value = _wrongAnswers.value?.plus(1)
+        }
+
+    }
+
+    fun nextQuestion() {
+        testQuestionsList.removeFirstOrNull()
+        _question.value = testQuestionsList.first()
+         _isAnswerCorrect.value = false
+    }
 }
