b749af1a15a4b6975c5b3e9f819b9711bcd96b08
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-30 17:29:54 +0200

    make home screen crollable when  used horizotally

--- app/src/main/java/com/example/licenciadearmas/HomeScreenFragment.kt
+++ app/src/main/java/com/example/licenciadearmas/HomeScreenFragment.kt
@@ -1,27 +1,30 @@
 package com.example.licenciadearmas
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.*
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-
 import com.example.licenciadearmas.ui.theme.LicenciaDeArmasTheme
-
 import com.example.licenciadearmas.ui.theme.Shapes
+import com.example.licenciadearmas.ui.theme.gunpPlay
 
 class HomeScreenFragment : Fragment() {
 
@@ -32,10 +35,12 @@ class HomeScreenFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 LicenciaDeArmasTheme {
+
                     HomeScreenContent(
                         onLearnClick = { findNavController().navigate(R.id.chooseSectionFragment) },
                         onTestClick = { findNavController().navigate(R.id.testFragment) }
                     )
+
                 }
             }
         }
@@ -45,41 +50,48 @@ class HomeScreenFragment : Fragment() {
 @Composable
 fun HomeScreenContent(
     onLearnClick: () -> Unit,
-    onTestClick: () -> Unit
+    onTestClick: () -> Unit,
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+
+            MainText(textRes = R.string.home_screen_main_text)
+
+            SecondaryText(textRes = R.string.home_screen_secondary_text)
+            Spacer(modifier = Modifier.weight(1f))
+
+            HomeScreenImage(imageRes = R.drawable.ic_path234)
+            Spacer(modifier = Modifier.weight(1f))
+
+            HomeScreenButton(
+                text = stringResource(id = R.string.learn_button_text),
+                onClick = onLearnClick
+            )
+
             Spacer(modifier = Modifier.height(25.dp))
 
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                MainText(textRes = R.string.home_screen_main_text)
+            HomeScreenButton(
+                text = stringResource(id = R.string.test_button_text),
+                onClick = onTestClick
+            )
 
-                SecondaryText(textRes = R.string.home_screen_secondary_text)
-                Spacer(modifier = Modifier.weight(1f))
-
-                HomeScreenImage(imageRes = R.color.logo_red)
-                Spacer(modifier = Modifier.weight(1f))
-
-                HomeScreenButton(
-                    text = stringResource(id = R.string.learn_button_text),
-                    onClick = onLearnClick
-                )
-
-                Spacer(modifier = Modifier.height(25.dp))
-
-                HomeScreenButton(
-                    text = stringResource(id = R.string.test_button_text),
-                    onClick = onTestClick
-                )
-
-                Spacer(modifier = Modifier.height(65.dp))
-            }
+            Spacer(modifier = Modifier.height(65.dp))
         }
     }
+
+
 }
 
 @Preview
@@ -111,21 +123,18 @@ fun HomeScreenButton(text: String, onClick: () -> Unit) {
 
 @Composable
 fun HomeScreenImage(imageRes: Int) {
+
     Image(
-        painter = painterResource(id = R.drawable.crosshair),
+        imageVector = ImageVector.vectorResource(id = imageRes),
         contentDescription = "logo",
         modifier = Modifier
-            .size(200.dp, 200.dp),
-
-        colorFilter = ColorFilter.tint(
-            colorResource(id = imageRes),
-        )
+            .size(280.dp, 280.dp)
     )
 }
 
 @Composable
 fun MainText(textRes: Int) {
-    Text(text = stringResource(id = textRes), fontSize = 40.sp)
+    Text(text = stringResource(id = textRes), fontSize = 40.sp, fontFamily = gunpPlay)
     Spacer(modifier = Modifier.height(15.dp))
 }
 
@@ -134,6 +143,7 @@ fun SecondaryText(textRes: Int) {
     Text(
         text = stringResource(id = textRes),
         fontSize = 24.sp,
-        color = colorResource(id = R.color.crosshair_red)
+        color = colorResource(id = R.color.logo_red),
+        fontFamily = gunpPlay
     )
 }
